3effa648ec6d8a60f54710156c0c1d204ee603b3
Cici <cici.mariani@hotmail.com>
2019-07-12 14:08:40 -0400

    testphp

---
+++ Web2/TP1php/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mariani
+ * Date: 2019-07-12
+ * Time: 14:01
+ */
+
+<footer id="bas de page" class="page-footer font-small  ">
+
+        <div class="footer-copyright text-center py-3 blackcolor">© 2019 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/"> Mariani.co</a>
+        </div>
+
+</footer>
---
+++ Web2/TP1php/header.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mariani
+ * Date: 2019-07-12
+ * Time: 14:07
+ */
+<head>
+<link id="header">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+
+<!-- META-->
+    <meta name="description" content="Les différentes activités à faire avec des bébés jeunes enfants et oyager autour du monde en famille" />
+    <meta http-equiv="author" content="Cynthia Mariani, Cici Mariani" />
+    <meta name="keywords" content="bébé, activités, enfants, été, sports, voyages, avion, train, musés, parcs, playgrounds" />
+    <title>Le temps été</title>
+<!--  FIN META-->
+
+
+<link rel="icon" type="image/png" href="img/StrollerIcon.png">
+
+<!-- navbar -->
+
+    <nav class="navbar navbar-expand-md sticky-top">
+        <a class="navbar-brand" href="index.html">Été 2019</a>
+
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                <i class="fas fa-baby-carriage"></i>
+            </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav mr-auto">
+                <div class="dropdown">
+                    <a class="nav-item dropdown nav-link dropdown-toggle" href="#" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Passions d'été>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="Roadtrips.html" target="_blank">Roadtrips</a>
+                        <a class="dropdown-item" href="Villes.html" target="_blank">Visite de villes</a>
+                        <a class="dropdown-item" href="Parcs.html" target="_blank">Stroller Parks</a>
+                    </div>
+                </div>
+
+<!-- section de droite-->
+            </div>
+                <div class="d-flex flex-row order-2 order-sm-3 align-items-center">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalYT">À l'Action</button>
+                    <a class="nav-item nav-link" href="form.html">Contact</a>
+                    <a class="nav-link" href="https://www.facebook.com/hi.mariani.co" target="www.facebook.com/hi.mariani.co">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a class="nav-link" href="https://twitter.com/" target="_blank">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a class="nav-link" href="https://www.instagram.com/?hl=fr-ca" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+        </div>
+    </nav>
+</head>
+<!-- fin navbar -->
